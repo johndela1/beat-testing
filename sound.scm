171550dff41af1d3f8706e697b785a916aa5f2e0
johnd <john@tvplus.com>
2013-10-20 15:24:01 -0700

    code formatting

--- sound.scm
+++ sound.scm
@@ -2,32 +2,34 @@
 
 (use srfi-18)
 
-(define *sample-rate* 8000)
+(define *SAMPLE-RATE* 8000)
 
 (define (sound freq duration)
-  (let* ((half_wavelength (/ *sample-rate* (* 2 freq)))
-         (len (* *sample-rate* duration)))
-    (oscilate len 255 half_wavelength)))
+  (oscilate (* *SAMPLE-RATE* duration)
+            (/ *SAMPLE-RATE* (* 2 freq))
+            0))
 
 
-(define (oscilate len amplitude half_wlen)
-  (if (= len 0) (quote ())
-		(if (= 0 (modulo (- 1 len) half_wlen))
-			(cons amplitude (oscilate (sub1 len) (bitwise-xor amplitude 255) half_wlen))
-			(cons amplitude (oscilate (sub1 len) amplitude half_wlen)))))
+(define (oscilate len half_wlen amplitude)
+  (if (= len 0)
+      (quote ())
+      (if (= 0 (modulo (- 1 len) half_wlen))
+          (cons amplitude (oscilate (sub1 len) half_wlen
+                                    (bitwise-xor amplitude 255)))
+          (cons amplitude (oscilate (sub1 len) half_wlen amplitude)))))
 
 
 
 (define (loop freq)
-	(map write-byte (sound freq 1))
-	(thread-sleep! 1)
-	(map write-byte (sound freq 1))
-	(thread-sleep! 1)
-	(map write-byte (sound freq 1))
-	(thread-sleep! 1)
-	(map write-byte (sound freq 1))
-	(thread-sleep! 2)
-	(loop freq))
+(map write-byte (sound freq 1))
+(thread-sleep! 1)
+(map write-byte (sound freq 1))
+(thread-sleep! 1)
+(map write-byte (sound freq 1))
+(thread-sleep! 1)
+(map write-byte (sound freq 1))
+(thread-sleep! 2)
+(loop freq))
 
 (define (output bytes)
 	(if (null? bytes) 0
@@ -35,5 +37,5 @@
 				  (write-byte (car bytes))
 				(output (cdr bytes)))))
 
-;(loop 100)
+(loop 100)
 ;(output (sound 1000 .01))
