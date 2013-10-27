6ef1a69cb55ffad76a8c0da785002e42460293d8
johnd <john@tvplus.com>
2013-10-26 18:13:05 -0700

    added clave_trainer.scm, uses sdl-mixer for async audio

---
+++ clave_trainer.scm
@@ -0,0 +1,51 @@
+#!/usr/bin/csi -s
+
+(use srfi-18)
+(use posix)
+(use sdl-mixer)
+(open-audio)
+
+
+(define *hz* 20)
+(define *jiffies-per-note* 10)
+
+;(define *song* '(B _ B B B _));two  over three
+;(define *song* '(B _ B _ _ B _ B _ B _ _)) ;rhumba clave
+(define *song* '(B _ B _ B _ _ B _ B _ _))  ;son clave
+(define *song-len* (length *song*))
+(define *boom* (load-sample "boom.vorbis"))
+
+
+
+(define (poll-keys)
+  (if (null? (file-select '(0) '() 0))
+      #f
+      (begin (write  '-) (read-byte))))
+
+(define (get-sub-division jiffies)
+  (modulo (/ jiffies *jiffies-per-note*) *song-len*))
+
+(define (check-to-play sub-div)
+  (display ".")
+  (if (= 0 sub-div) (display 'one))
+  (eq? 'B (list-ref *song* sub-div)))
+
+(define (sub-division? jiffies)
+  (= 0 (modulo jiffies *jiffies-per-note*)))
+
+(define (check-sub-division jiffies)
+  (if (and (sub-division? jiffies)
+      (check-to-play (get-sub-division jiffies)))
+      (begin
+        (play-sample *boom*)
+        (write 'boom))))
+
+
+(define (loop jiffies)
+  (check-sub-division jiffies)
+  (poll-keys)
+  (print "")
+  (thread-sleep! (/ 1 *hz*))
+  (loop (add1 jiffies)))
+
+(loop 0)
