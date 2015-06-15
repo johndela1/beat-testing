66784432c79680949436c39621e601ca8a01b50c
John de la Garza <john.garza@rallyhealth.com>
2015-06-15 13:19:50 -0700

    beat_testing: basic working version

--- beat_testing.scm
+++ beat_testing.scm
@@ -1,12 +1,13 @@
 #!/usr/local/bin/csi -s
 
+(use posix)
 (use srfi-1)
 
 (define (make-ts l)
 	(define (make-ts-iter l t)
 		(cond
 			((null? l) '())
-			(( = 0 (car l)) (make-ts-iter (cdr l) (add1 t)))
+			((eq? 0 (car l)) (make-ts-iter (cdr l) (add1 t)))
 			(else (cons t (make-ts-iter (cdr l) (add1 t))))))
 	(make-ts-iter l 0))
 
@@ -20,17 +21,34 @@
 			(map round
 				(map (lambda (x) (* x factor)) l)))))
 	
-(define (errors ref input)
-	(map (lambda (pair) ((zip ref input))
+(define (pass? ref input)
+		(not (any (lambda (x) (print x) (> x 60))
+			(map (lambda (pair) (abs (- (car pair) (cadr pair))))
+			     (zip ref input)))))
 
-(print (make-ts '(1 0 0 0 1 0 0 0 1)))
-(print (map (lambda (x) (* (+ 100 x) 10)) (make-ts '(1 0 0 0 1 0 0 0 1))))
-(print (map (lambda (x) (+ (* 10 x) 100)) (make-ts '(1 0 0 0 1 0 0 0 1))))
+(define (read-pattern n samples)
+	(cond
+		((= n 0) (map inexact->exact (reverse samples)))
+		(else
+			(file-select '(0) '() 3)
+			(let ((ts (current-milliseconds)))
+				(read-byte)
+				(read-pattern (sub1 n) (cons ts samples))))))
+(define (align ref input)
+	(cond
+		(else (print "This wont be trivial"))))
 
-(print (start-at-zero '(0 1 2)))
-(print (scale (start-at-zero '(10 14 18)) 2))
-(print (scale (start-at-zero '(100 140 181)) 2))
-(print (scale (start-at-zero (make-ts '(1 0 0 0 1 0 0 0 1))) 2))
+;(print (scale (start-at-zero (make-ts '(1 0 0 0 1 0 0 0 1))) 1000))
+;(print (map (lambda (x) (* (+ 100 x) 10)) (make-ts '(1 0 0 0 1 0 0 0 1))))
+;(print (map (lambda (x) (+ (* 10 x) 100)) (make-ts '(1 0 0 0 1 0 0 0 1))))
 
+(define (go)
+	(let (
+		(input (scale
+			(start-at-zero (read-pattern 8 '())) 1000))
+		(ref (scale
+			(make-ts '(x 0 x 0 x 0 x 0 0 x 0 x 0 x 0 x)) 1000)))
 
-(print (errors '(1 2 3) '(1 2 4)))
+		(print (pass? ref input)))
+	(loop))
+(loop)
